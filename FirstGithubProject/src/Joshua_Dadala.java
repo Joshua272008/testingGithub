58606d02c0322e721698f563cf4a5343f93f573e
allenhouu <126ahou@frhsd.com>
2024-02-20 13:56:06 -0500

    Update Joshua_Dadala.java

--- FirstGithubProject/src/Joshua_Dadala.java
+++ FirstGithubProject/src/Joshua_Dadala.java
@@ -1,4 +1,4 @@
 public class Joshua_Dadala {
     System.out.println("Hi Joshua Dadala, from ben");
-    System.out.println("Hi Joshua, from Allen");
+    System.out.println("Hi Ben, from Allen");
 }
