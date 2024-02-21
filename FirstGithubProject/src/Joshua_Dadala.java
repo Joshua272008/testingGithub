a81c2512f2d289c104f23271e8e2535f031cd998
allenhouu <126ahou@frhsd.com>
2024-02-21 13:12:24 -0500

    Hi Josh, Ben, and Stephen.

--- FirstGithubProject/src/Joshua_Dadala.java
+++ FirstGithubProject/src/Joshua_Dadala.java
@@ -2,5 +2,6 @@ public class Joshua_Dadala {
     public static void main(String[] args) {
         System.out.print("Hi Joshua Dadala, from ben");
         System.out.print("Hi Joshua Dadala, from Stephen.");
+        System.out.print("Hi Joshua, Ben, and Stephen, from Allen.");
     }
 }
