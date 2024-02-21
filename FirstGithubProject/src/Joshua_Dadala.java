8873088d7e669c2a80a16d654a81fe39c510ee3e
Joshua272008 <113116525+Joshua272008@users.noreply.github.com>
2024-02-21 13:44:40 -0500

    Revert "Hi Josh, Ben, and Stephen."
    This reverts commit a81c2512f2d289c104f23271e8e2535f031cd998.

--- FirstGithubProject/src/Joshua_Dadala.java
+++ FirstGithubProject/src/Joshua_Dadala.java
@@ -2,6 +2,5 @@ public class Joshua_Dadala {
     public static void main(String[] args) {
         System.out.print("Hi Joshua Dadala, from ben");
         System.out.print("Hi Joshua Dadala, from Stephen.");
-        System.out.print("Hi Joshua, Ben, and Stephen, from Allen.");
     }
 }
